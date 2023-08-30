a79480e4e0fc358eefdc8da2c3e5e197c8bf2071
Martin Ambruš <martin@martinambrus.com>
2023-08-30 20:18:55 +0200

    fix: missing topic for MQ

--- IMessageQueue.ts
+++ IMessageQueue.ts
@@ -6,11 +6,12 @@ export interface IMessageQueue {
   /**
    * Sends message to the message queue.
    *
+   * @param { string } topic    Topic to sent the message to.
    * @param { Object } msg      Message data to publish. Will be converted into a JSON string.
    * @param { string } trace_id Optional. ID of the tracing software's root span, so we can continue
    *                            tracing the request as it flows through the relevant microservices.
    * @return Promise<void>
    */
-  send( message: Object, trace_id?: string ): Promise<void>;
+  send( topic: string, message: Object, trace_id?: string ): Promise<void>;
 
 }
